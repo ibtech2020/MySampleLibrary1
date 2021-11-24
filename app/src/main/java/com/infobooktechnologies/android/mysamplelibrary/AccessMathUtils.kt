97a72bc10b168705ce8edbf67ad017b6db626d09
ibtech2020 <infobooktechnologies@gmail.com>
2021-11-24 15:51:29 -0500

    Gradle Initial commit

--- app/src/main/java/com/infobooktechnologies/android/mysamplelibrary/AccessMathUtils.kt
+++ app/src/main/java/com/infobooktechnologies/android/mysamplelibrary/AccessMathUtils.kt
@@ -3,5 +3,5 @@ package com.infobooktechnologies.android.mysamplelibrary
 import com.infobooktechnologies.android.mathoperation.performAdd
 
 fun main() {
-    print(performAdd(2,4))
+    print(performAdd(2,7))
 }
